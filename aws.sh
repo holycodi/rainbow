4641998947d7a0b8c03f6341d03b988d4d6129a3
holycodi <victor.biga@gmail.com>
2018-05-22 23:19:00 +0100

    added decsription and update repo command

--- aws.sh
+++ aws.sh
@@ -1,1 +1,10 @@
-touch aws.txt
+#!/bin/bash
+#Script Name	:  AWS + NGINX                                                                                            
+#Description	:  This will install linux on aws and install and configure NGINX                                                                               
+#Author       	:Victor Biga                                                
+#Email         	:victor.biga@gmail.com                                           
+#Version info   : 1.0
+
+#Run the update to make sure repository contains all the latest apps
+sudo apt-get update
+
