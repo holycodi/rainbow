9a2fc6c4b3631d8da4858a1e6f2b886937cd18b3
holycodi <victor.biga@gmail.com>
2018-05-22 23:29:26 +0100

    added text_file.txt with some text

--- aws.sh
+++ aws.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#Script Name	:  AWS + NGINX                                                                                            
-#Description	:  This will install linux on aws and install and configure NGINX                                                                               
+#Script Name	:AWS + NGINX                                                                                            
+#Description	:This will install linux on aws and install and configure NGINX                                                                               
 #Author       	:Victor Biga                                                
 #Email         	:victor.biga@gmail.com                                           
 #Version info   : 1.0
 
 #Run the update to make sure repository contains all the latest apps
 sudo apt-get update
+
+#Create a test file in users home directory
+cat > text_file.txt 
+some text in the text_file.txt
 
