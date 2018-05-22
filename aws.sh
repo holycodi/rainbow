1462f95f26dd6a34904ddf20dfa6125ee85a7979
holycodi <victor.biga@gmail.com>
2018-05-22 23:44:56 +0100

    added sudo to line 9

--- aws.sh
+++ aws.sh
@@ -6,7 +6,7 @@
 #Version info   : 1.0
 
 #Run the update to make sure repository contains all the latest apps
-yum update
+sudo yum update
 
 #Create a test file in users home directory
 cat > text_file.txt 
